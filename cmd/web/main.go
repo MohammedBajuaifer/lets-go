cb56d0b21b79deab349b5c9f646a0f6dfd33b948
Mohammed Bajuaifer <mohamadbajuaifer@gmail.com>
2024-09-21 23:19:51 +0300

    update

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type application struct {
 	logger *slog.Logger
 }
 
+const createTable string = `
+	CREATE TABLE IF NOT EXISTS snippets (
+	id INTEGER NOT NULL PRIMARY KEY,
+	time DATETIME NOT NULL,
+	description TEXT
+	)
+`
+
 func main() {
+
+	db, err := sql.Open("sqlite3", "test.db")
+
+	if err != nil {
+		slog.Error(err.Error())
+	}
+
+	_, err = db.Exec(createTable)
+	if err != nil {
+		slog.Error(err.Error())
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
 
 	loggerHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})
 
@@ -25,7 +56,7 @@ func main() {
 
 	logger.Info("Starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 
 	logger.Error(err.Error())
 
---
+++ internal/db.go
@@ -0,0 +1,13 @@
+package internal
+
+// const createTable string = `
+// 	CREATE TABLE IF NOT EXISTS snippets (
+// 	id INTEGER NOT NULL PRIMARY KEY,
+// 	time DATETIME NOT NULL,
+// 	description TEXT
+// 	)
+// `
+
+// func (app *application) createTable() {
+
+// }
